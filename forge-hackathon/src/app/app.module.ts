49e6e6e2cd585c3d4f2acdadb94bea9d5ab76140
Hamdi Damar <hamdi.damar@hotmail.com>
2020-11-09 21:33:24 +0300

    angular-10 app created and login, projects, viewer component added

---
+++ forge-hackathon/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ViewerComponent } from './viewer/viewer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProjectsComponent,
+    ViewerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
